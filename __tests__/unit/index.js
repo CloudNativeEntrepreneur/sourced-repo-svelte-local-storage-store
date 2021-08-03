b46712067dea02e22470d60ce19b37508f56dbec
Patrick Lee Scott <pat@patscott.io>
2021-08-02 23:01:51 -0500

    feat: move eventemitter3 out of peer deps + fix lint no-async-promise-executor issue
    Signed-off-by: Patrick Lee Scott <pat@patscott.io>

--- __tests__/unit/index.js
+++ __tests__/unit/index.js
@@ -1,7 +1,27 @@
 import { Repository } from 'index'
+import { Entity } from 'sourced'
+
+class Example extends Entity {
+  constructor(snapshot, events) {
+    super()
+    this.value = 0
+    this.rehydrate(snapshot, events)
+  }
+
+  increment() {
+    this.value++
+    this.digest('increment')
+    this.enqueue('incremented', this)
+  }
+}
 
 describe('sourced-repo-svelte-local-storage-store', () => {
   it('should exist', () => {
     expect(Repository).toBeDefined()
+
+    const repo = new Repository(Example)
+
+    expect(repo.get).toBeDefined
+    expect(repo.commit).toBeDefined
   })
 })
