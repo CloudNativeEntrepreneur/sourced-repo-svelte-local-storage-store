6703e0f3f1757334b819a1b9b690cf063a9179db
Patrick Lee Scott <pat@patscott.io>
2021-08-31 10:43:29 -0400

    fix: cleanup unneeded promises
    Signed-off-by: Patrick Lee Scott <pat@patscott.io>

--- src/Repository.ts
+++ src/Repository.ts
@@ -51,23 +51,10 @@ export class Repository extends EventEmitter {
     this.emit("ready");
   }
 
-  async commit(entity) {
+  commit(entity) {
     log(`committing ${this.EntityType.name} for id ${entity.id}`);
-
-    try {
-      await this._commitEvents(entity);
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-
-    try {
-      await this._commitSnapshots(entity);
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-
+    this._commitEvents(entity);
+    this._commitSnapshots(entity);
     this._emitEvents(entity);
     return this;
   }
@@ -77,91 +64,79 @@ export class Repository extends EventEmitter {
   }
 
   _getByIndex(index, value) {
-    const self = this;
+    log(`getting ${this.EntityType.name} where "${index}" is "${value}"`);
 
-    return new Promise((resolve, reject) => {
-      log(`getting ${this.EntityType.name} where "${index}" is "${value}"`);
+    const allSnapshots: unknown[] = get(this.snapshots) as any[];
+    const snapshots = allSnapshots.filter(
+      (snapshot) => snapshot[index] === value
+    );
+    const snapshot: any = snapshots[0];
 
-      const allSnapshots: unknown[] = get(this.snapshots) as any[];
-      const snapshots = allSnapshots.filter(
-        (snapshot) => snapshot[index] === value
+    const allEvents: unknown[] = get(this.events) as any[];
+    const events: any[] = allEvents
+      .filter((event: any) => event[index] === value)
+      .filter((event: any) =>
+        snapshot ? event.version > snapshot.version : true
       );
-      const snapshot: any = snapshots[0];
 
-      const allEvents: unknown[] = get(this.events) as any[];
-      const events: any[] = allEvents
-        .filter((event: any) => event[index] === value)
-        .filter((event: any) =>
-          snapshot ? event.version > snapshot.version : true
-        );
+    log({ snapshots, snapshot, events });
 
-      log({ snapshots, snapshot, events });
+    // if (snapshot && snapshot._id) delete snapshot._id
+    if (!snapshot && !events.length) {
+      return null;
+    }
 
-      // if (snapshot && snapshot._id) delete snapshot._id
-      if (!snapshot && !events.length) {
-        return resolve(null);
-      }
+    const id = index === "id" ? value : snapshot ? snapshot.id : events[0].id;
 
-      const id = index === "id" ? value : snapshot ? snapshot.id : events[0].id;
-
-      const entity = self._deserialize(id, snapshot, events);
-      return resolve(entity);
-    });
+    const entity = this._deserialize(id, snapshot, events);
+    return entity;
   }
 
   _commitEvents(entity) {
-    return new Promise((resolve, reject) => {
-      if (entity.newEvents.length === 0) return resolve(null);
+    if (entity.newEvents.length === 0) return null;
 
-      if (!entity.id) {
-        return reject(
-          new Error(
-            `Cannot commit an entity of type ${this.EntityType} without an [id] property`
-          )
-        );
-      }
+    if (!entity.id) {
+      throw new Error(
+        `Cannot commit an entity of type ${this.EntityType} without an [id] property`
+      );
+    }
 
-      const newEvents = entity.newEvents;
-      newEvents.forEach((event) => {
-        this.indices.forEach(function (index) {
-          event[index] = entity[index];
-        });
+    const newEvents = entity.newEvents;
+    newEvents.forEach((event) => {
+      this.indices.forEach(function (index) {
+        event[index] = entity[index];
       });
-
-      this.events.update((previousEvents) => [...previousEvents, ...newEvents]);
-      entity.newEvents = [];
-
-      log(`committed ${this.EntityType.name}.events for id ${entity.id}`);
-
-      resolve(entity);
     });
+
+    this.events.update((previousEvents) => [...previousEvents, ...newEvents]);
+    entity.newEvents = [];
+
+    log(`committed ${this.EntityType.name}.events for id ${entity.id}`);
+
+    return entity;
   }
 
   _commitSnapshots(entity) {
-    const self = this;
+    if (
+      this.forceSnapshot ||
+      entity.version >= entity.snapshotVersion + this.snapshotFrequency
+    ) {
+      const snapshot = entity.snapshot();
+      // put new one at the beginning for premptive sorting
+      this.snapshots.update((previousSnapshots) => [
+        snapshot,
+        ...previousSnapshots,
+      ]);
 
-    return new Promise((resolve, reject) => {
-      if (
-        self.forceSnapshot ||
-        entity.version >= entity.snapshotVersion + self.snapshotFrequency
-      ) {
-        const snapshot = entity.snapshot();
-        // put new one at the beginning for premptive sorting
-        this.snapshots.update((previousSnapshots) => [
-          snapshot,
-          ...previousSnapshots,
-        ]);
+      log(
+        `committed ${this.EntityType.name}.snapshot for id ${entity.id}`,
+        snapshot
+      );
 
-        log(
-          `committed ${self.EntityType.name}.snapshot for id ${entity.id}`,
-          snapshot
-        );
-
-        return resolve(entity);
-      } else {
-        return resolve(entity);
-      }
-    });
+      return entity;
+    } else {
+      return entity;
+    }
   }
 
   _deserialize(id, snapshot, events) {
@@ -173,13 +148,11 @@ export class Repository extends EventEmitter {
 
   _emitEvents(entity) {
     log("emitting events");
-    const self = this;
-
     const eventsToEmit = entity.eventsToEmit;
     entity.eventsToEmit = [];
-    eventsToEmit.forEach(function (eventToEmit) {
+    eventsToEmit.forEach((eventToEmit) => {
       const args = Array.prototype.slice.call(eventToEmit);
-      self.EntityType.prototype.emit.apply(entity, args);
+      this.EntityType.prototype.emit.apply(entity, args);
     });
 
     log("emitted local events for id %s", entity.id);
